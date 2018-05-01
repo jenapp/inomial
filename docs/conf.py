98e1f415f0b68f9cbd548d4e52c002be6fe5fc75
Jennifer Appleby <33709515+jenapp@users.noreply.github.com>
2018-05-01 17:12:16 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,8 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = ["_themes"]
+html_theme_path = ["_themes", ]
+html_logo = 'logo.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
